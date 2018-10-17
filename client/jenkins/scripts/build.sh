53b3899dcc3fec59ff500e1cce48d2e374bac077
kevin xie <kevin.xie@cargosmart.com>
2018-10-17 13:14:57 +0800

    update build.sh build path

--- client/jenkins/scripts/build.sh
+++ client/jenkins/scripts/build.sh
@@ -2,7 +2,7 @@
 echo Building vuejs:build
 
 # build a image for package vue source
-docker build -t vuejs:build -f ../../Dockerfile.build ../../
+docker build -t vuejs:build -f ./client/Dockerfile.build ./client/
 
 # run up a container and copy the build result to ./dist, then delete this container
 docker container create --name extract vuejs:build 
@@ -11,7 +11,7 @@ docker container rm -f extract
 
 echo Building fm_prs:latest
 # start build a image for vue with nginx
-docker build --no-cache -t fm_prs:latest -f ../../Dockerfile ../../
+docker build --no-cache -t fm_prs:latest -f ./client/Dockerfile ./client/
 
 # delete the build result after all done.
 rm -rf ./dist

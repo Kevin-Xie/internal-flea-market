8febc0f51f5e16f2d7abea905afe7e8b4cbf45c1
kevin xie <kevin.xie@cargosmart.com>
2018-10-22 22:58:06 +0800

    add push images

---
+++ client/jenkins/scripts/push.sh
@@ -0,0 +1,3 @@
+docker tag fm_prs 10.0.2.15:5000/fm/fm_prs:latest
+
+docker push 10.0.2.15:5000/fm/fm_prs:latest
---
+++ server/jenkins/scripts/push.sh
@@ -0,0 +1,4 @@
+
+docker tag fm_dom 10.0.2.15:5000/fm/fm_dom:latest
+
+docker push 10.0.2.15:5000/fm/fm_dom:latest
